35f0c8983f152dab17f7afae10c26861a39f0e0e
HarryMiller <HarryMillerMail@googlemail.com>
2016-03-26 17:44:05 +0100

    another date check

--- test/date_test.js
+++ test/date_test.js
@@ -12,6 +12,21 @@ describe('DateTime / MongoDb', function() {
 		assert.equal(19, dt.getUTCMinutes());
 		assert.equal(28, dt.getUTCSeconds());
 	});
+
+
+	it ('should read the correct time', function() {
+		var str = "2016-03-26T17:10:44.000Z";
+		var dt = new Date(str);
+		assert.equal(2016, dt.getUTCFullYear());
+		assert.equal(3, dt.getUTCMonth()+1);
+		assert.equal(26, dt.getUTCDate());
+		assert.equal(17, dt.getUTCHours());
+		assert.equal(10, dt.getUTCMinutes());
+		assert.equal(44, dt.getUTCSeconds());
+	});
+
+
+
 });
 
 
